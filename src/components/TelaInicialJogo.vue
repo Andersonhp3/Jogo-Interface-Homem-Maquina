cfc39d35b90d6777fb8f8dae00f024f5cd458ef5
Andersonhp3 <arhp1591@gmail.com>
2022-08-22 22:41:38 -0300

    feat: inical code

---
+++ src/components/TelaInicialJogo.vue
@@ -0,0 +1,30 @@
+<template>
+
+<div>
+<h3>Babylon Examples</h3>
+<canvas></canvas>
+
+</div>
+
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { FaseUm } from '@/Fases/FaseUm';
+export default defineComponent({
+  name: 'TelaInicialJogo',
+  mounted(){
+    const canvas = document.querySelector("canvas")!;
+    new FaseUm(canvas);
+  }
+  
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+canvas {
+width:70%;
+height:70%;
+}
+</style>
