18fdfe5d7307da637314e98abcb8eed010de532a
Andersonhp3 <arhp1591@gmail.com>
2022-09-22 18:57:49 -0300

    feat: animation move character

--- src/Fases/FaseUm.ts
+++ src/Fases/FaseUm.ts
@@ -8,18 +8,23 @@ import {
   SceneLoader,
   CubeTexture,
   ArcRotateCamera,
+  PhysicsImpostor,
+  CannonJSPlugin,
+  AbstractMesh,
+  Animation,
+  Mesh,
 } from "@babylonjs/core";
 import "@babylonjs/loaders";
+import * as CANNON from "cannon";
 
 export class FaseUm {
   scene: Scene;
   engine: Engine;
+  character: AbstractMesh;
 
   constructor(private canvas: HTMLCanvasElement) {
     this.engine = new Engine(this.canvas, true);
     this.scene = this.CreateScene();
-    //this.CreateGround();
-    //this.CreateBarrel();
     this.CreateFaseUm();
     this.CreateCharacter();
 
@@ -30,19 +35,19 @@ export class FaseUm {
 
   CreateScene(): Scene {
     const scene = new Scene(this.engine);
-    // const camera = new FreeCamera(
-    //   "camera",
-    //   new Vector3(0, 1, -5),
-    //   this.scene
-    // );
-    // camera.attachControl();
-    // camera.speed = 0.25;
-
-    const camera = new ArcRotateCamera("Camera", 0, 0, 5, new Vector3(0, 0.25, 0), scene);
-
-    camera.setPosition(new Vector3(-2, 3, 2.5));
+    const camera = new FreeCamera(
+      "camera",
+      new Vector3(0, 1, -5),
+      this.scene
+    );
     camera.attachControl();
-    camera.speed = 0.18;
+    camera.speed = 0.25;
+
+    // const camera = new ArcRotateCamera("Camera", 0, 0, 5, new Vector3(0, 0.25, 0), scene);
+
+    // camera.setPosition(new Vector3(-2, 3, 2.5));
+    // camera.attachControl();
+    // camera.speed = 0.18;
 
     const hemiLight = new HemisphericLight(
       "hemiLight",
@@ -51,6 +56,11 @@ export class FaseUm {
     );
 
     hemiLight.intensity = 3;
+
+    scene.enablePhysics(
+      new Vector3(0, -9.81, 0),
+      new CannonJSPlugin(true, 10, CANNON)
+    );
 
     return scene;
   }
@@ -63,12 +73,12 @@ export class FaseUm {
     );
   }
 
-
   async CreateCharacter(): Promise<void> {
     const { meshes, animationGroups } = await SceneLoader.ImportMeshAsync(
       "",
       "./models/",
-      "character.glb"
+      "character.glb",
+      this.scene
     );
 
     // meshes[0].rotate(Vector3.Up(), Math.PI);
@@ -76,13 +86,46 @@ export class FaseUm {
     meshes[0].scaling.y = 0.45;
     meshes[0].scaling.z = 0.45;
 
-    meshes[0].position = new Vector3(1.2,0.20,-0.1);
-    meshes[0].rotation = new Vector3(0,5,0);
+    meshes[0].position = new Vector3(1.2,0,-0.15);
+    meshes[0].position.y = 0.20
+    meshes[0].rotation = new Vector3(0, 4.70, 0);
 
-    console.log("animation groups", animationGroups);
-
-    animationGroups[0].stop();
-
-    animationGroups[2].play(true);
+    animationGroups[3].play(true);
+    this.character =  meshes[0];
   }
+
+  CreateAnimations(): void {
+    const moveFrames = [];
+    const fps = 60;
+
+    const moveAnim = new Animation(
+      'moveAnim', 
+      'position.x', 
+      fps, 
+      Animation.ANIMATIONTYPE_FLOAT, 
+      Animation.ANIMATIONLOOPMODE_CONSTANT
+      );
+
+      moveFrames.push({frame:0, value:0});
+      moveFrames.push({frame:180, value: 0.4});
+
+      moveAnim.setKeys(moveFrames);
+
+      this.character.animations.push(moveAnim);
+
+      this.scene.beginAnimation(this.character, 0, 180)
+  }
+
+
+  // CreateFloorImpostor(): void {
+  //   const ground = MeshBuilder.CreateGround('ground', { width: 2.5, height: 3 })
+  //   ground.position.y = 0.2;
+  //   ground.position.x = 0.35;
+  //   ground.isVisible = false;
+  //   ground.physicsImpostor = new PhysicsImpostor(
+  //     ground,
+  //     PhysicsImpostor.BoxImpostor,
+  //     { mass: 0, restitution: 0 }
+  //   );
+  // }
 }
